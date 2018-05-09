80664f859a4ed2aed887e14bcc5f73b5f0872257
Oliver Chi <oliver.chi.ict@icloud.com>
2018-05-09 17:48:57 +1000

    data core init

--- DepressiveDetector/DetailViewController.swift
+++ DepressiveDetector/DetailViewController.swift
@@ -17,7 +17,8 @@ class DetailViewController: UIViewController {
         // Update the user interface for the detail item.
         if let detail = detailItem {
             if let label = detailDescriptionLabel {
-                label.text = detail.timestamp!.description
+                //label.text = detail.timestamp!.description
+                label.text = detail.name
             }
         }
     }
@@ -33,7 +34,7 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: Event? {
+    var detailItem: Child? {
         didSet {
             // Update the view.
             configureView()
--- DepressiveDetector/MasterViewController.swift
+++ DepressiveDetector/MasterViewController.swift
@@ -13,6 +13,10 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
+    
+    //Give cell title and subtitle
+    //let child: [String] = ["child1","child2","child3","child4","child5"]
+    //let twitter:[String] = ["twitter1","twitter2","twitter3","twitter4","twitter5"]
 
 
     override func viewDidLoad() {
@@ -46,10 +50,16 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         self.present(newViewController, animated: true, completion: nil)
         
         let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
+        //let newEvent = Event(context: context)
+        //Set up new tuple in SQL Table Child
+        let newChild = Child(context: context)
              
         // If appropriate, configure the new managed object.
-        newEvent.timestamp = Date()
+        //newEvent.timestamp = Date()
+        //Set attributes in tuple
+        newChild.name = "child"
+        newChild.twitterAccount = "twitter"
+        
 
         // Save the context.
         do {
@@ -90,8 +100,11 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let event = fetchedResultsController.object(at: indexPath)
-        configureCell(cell, withEvent: event)
+        let child = fetchedResultsController.object(at: indexPath)
+    
+        //configure cell func
+        configureCell(cell, withEvent: child)
+        
         return cell
     }
 
@@ -116,24 +129,27 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         }
     }
 
-    func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
+    func configureCell(_ cell: UITableViewCell, withEvent child: Child) {
+        //cell.textLabel!.text = event.timestamp!.description
+        //Set cell title and subtitle
+        cell.textLabel?.text = child.name
+        cell.detailTextLabel?.text = child.twitterAccount
     }
 
     // MARK: - Fetched results controller
 
-    var fetchedResultsController: NSFetchedResultsController<Event> {
+    var fetchedResultsController: NSFetchedResultsController<Child> {
         if _fetchedResultsController != nil {
             return _fetchedResultsController!
         }
-        
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
+       
+        let fetchRequest: NSFetchRequest<Child> = Child.fetchRequest()
         
         // Set the batch size to a suitable number.
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
@@ -154,7 +170,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         
         return _fetchedResultsController!
     }    
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
+    var _fetchedResultsController: NSFetchedResultsController<Child>? = nil
 
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
@@ -178,9 +194,9 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             case .delete:
                 tableView.deleteRows(at: [indexPath!], with: .fade)
             case .update:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Child)
             case .move:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Child)
                 tableView.moveRow(at: indexPath!, to: newIndexPath!)
         }
     }
