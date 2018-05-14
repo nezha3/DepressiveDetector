a8a6de37b46d6ca376e0472da67419db7401f827
Oliver Chi <oliver.chi.ict@icloud.com>
2018-05-14 20:57:37 +1000

    update

--- DepressiveDetector/DetailViewController.swift
+++ DepressiveDetector/DetailViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import CoreData
 
-class DetailViewController: UIViewController {
-    
+class DetailViewController: UIViewController{
+
+
+    @IBOutlet weak var chart: BasicBarChart!
     @IBOutlet weak var childName: UILabel!
     
     func configureView() {
@@ -21,13 +23,40 @@ class DetailViewController: UIViewController {
             }
         }
         
-        //add more details for items
+        //Draw chart
+        let dataEntries = generateDataEntries()
+        chart.dataEntries = dataEntries
+        //barChart.dataEntries = dataEntries
+    }
+    
+    func generateDataEntries() -> [BarEntry] {
+        let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
+        var result: [BarEntry] = []
+        for i in 0..<20 {
+            let value = (arc4random() % 90) + 10
+            let height: Float = Float(value) / 100.0
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d MMM"
+            var date = Date()
+            date.addTimeInterval(TimeInterval(24*60*60*i))
+            result.append(BarEntry(color: colors[i % colors.count], height: height, textValue: "\(value)", title: formatter.string(from: date)))
+        }
+        return result
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        // Redraw chart on rotation
+        //chart.setNeedsDisplay()
     }
 
     override func didReceiveMemoryWarning() {
